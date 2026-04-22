73ddc168b24689ae0766a7ef96c52fbdec2158c7
Samay Dhawan <samay58@gmail.com>
2026-04-22 13:24:02 -0400

    chore(fonts): load v2 type families from Google Fonts

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -16,5 +16,14 @@
   });
 </script>
 
+<svelte:head>
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous" />
+  <link
+    rel="stylesheet"
+    href="https://fonts.googleapis.com/css2?family=EB+Garamond:ital,wght@1,700&family=IBM+Plex+Mono:wght@400;700&family=IBM+Plex+Sans:wght@500;700&family=IM+Fell+English&family=Old+Standard+TT:wght@400;700&display=swap"
+  />
+</svelte:head>
+
 {@render children()}
 <DialRoot />
